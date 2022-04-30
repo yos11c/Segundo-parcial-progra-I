6c49a955128985fd0e161110a50e3b465270e9c4
yos11c <ycontrerasc3@miumg.edu.gt>
2022-04-30 21:20:32 +0000

    fallaid

--- JavaApplication16/src/javaapplication16/Cuadrado.java
+++ JavaApplication16/src/javaapplication16/Cuadrado.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package javaapplication16;
+import javax.swing.JOptionPane;
+
 
 /**
  *
  * @author Yosmeri C. Cruz
  */
-public class Cuadrado {
+public class Cuadrado extends FigurasGeometricas {
     
+    @Override
+    public void AreaF ()
+    {
+        System.out.println("Area del Cuadrado");
+        
+        lado = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el Lado del Cuadrado"));
+        
+        Area = lado * lado;
+        
+        System.out.println("Lado: " +lado);
+        System.out.println("Formula: LADO * LADO");
+        
+        System.out.println("El area del cuadrado es: " +Area);
+    }
 }
--- JavaApplication16/src/javaapplication16/Principal.java
+++ JavaApplication16/src/javaapplication16/Principal.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication16;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -10,11 +11,59 @@ package javaapplication16;
  */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int op;
+        do
+        {
+            op = Integer.parseInt(JOptionPane.showInputDialog(null,"CALCULAR EL AREA DE LAS FIGURAS GEOMETRICAS"));
+                  " seleccione una opcion: \n"
+                  " 1. Triangulo\n"
+                  " 2. Cuadrado\n"
+                  " 3. Circulo\n"
+                  " 4. Rectangulo\n"
+                  " 5. Salir"));
+                
+            switch (op)
+            {
+                case 1 -> {
+                    Triangulo Nia = new Triangulo ();
+                    Nia.AreaF ();
+                    System.out.println("");
+                }
+                    
+                case 2 -> {
+                    Cuadrado Nia1 = new Cuadrado();
+                    Nia1.AreaF();
+                    System.out.println ("");
+                }
+                    
+                case 3 -> {
+                    Circulo Nia2 = new Circulo ();
+                    Nia2.AreaF();
+                    System.out.println("");
+                }
+                case 4 -> {
+                    Rectangulo Nia3 = new Rectangulo ();
+                    Nia3.AreaF();
+                    System.out.println("");
+                }
+                    
+                case 5 -> {
+                    JOptionPane.showMessageDialog(null, "QUE TENGA BUEN DIA");System.exit(0);
+                }
+                    
+                default -> {
+                        JOptionPane.showMessageDialog(null, "Elige una opcion valida");
+                        System.out.println("");
+                }
+                
+                    
+            }
+                
+            
+        }while(op!=5);
     }
     
 }
